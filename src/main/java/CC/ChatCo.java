b3220c3c0ee568cfb91343d1a912a8443bc8fb9a
maxxie114 <maxxie114@gmail.com>
2018-11-03 16:45:56 -0700

    1. Reformat to Google Style 2. move plugin.yml outside 3. remove some unnecessary codes

--- src/main/java/CC/ChatCo.java
+++ src/main/java/CC/ChatCo.java
@@ -1,38 +1,29 @@
 package CC;
 
-import com.google.googlejavaformat.java.Formatter;
-
-import cn.nukkit.utils.TextFormat;
-import cn.nukkit.Server;
-import cn.nukkit.Player;
-import cn.nukkit.event.Listener;
-import cn.nukkit.plugin.Plugin;
-import cn.nukkit.plugin.PluginManager;
-import cn.nukkit.plugin.PluginBase;
-import cn.nukkit.event.player.PlayerChatEvent;
-import cn.nukkit.event.player.PlayerMessageEvent;
 import cn.nukkit.event.EventHandler;
 import cn.nukkit.event.Listener;
+import cn.nukkit.event.player.PlayerChatEvent;
+import cn.nukkit.plugin.PluginBase;
+import cn.nukkit.utils.TextFormat;
 
 public class ChatCo extends PluginBase implements Listener {
-    
-    public ChatCo() {}
-    
-    public void onLoad(){
-        getLogger().info("Loading ChatCo");
+
+
+  public void onLoad() {
+    this.getLogger().info("Loading ChatCo");
+  }
+
+  public void onEnable() {
+    this.getServer().getPluginManager().registerEvents(this, this);
+    this.getLogger().info("Enabled ChatCo");
+  }
+
+  @EventHandler
+  public void onChat(PlayerChatEvent e) {
+    String msg = e.getMessage();
+    if (msg.startsWith(">")) {
+      msg = TextFormat.GREEN + msg;
+      e.setMessage(msg);
     }
-    
-    public void onEnable(){
-        getServer().getPluginManager().registerEvents(this, this);
-        getLogger().info("Enabled ChatCo");
-    }
-    
-    @EventHandler
-    public void onChat(PlayerChatEvent e) {
-        String msg = e.getMessage();
-        if (msg.startsWith(">")) {
-            msg = TextFormat.GREEN + msg;
-            e.setMessage(msg);
-        }
-    }
+  }
 }
